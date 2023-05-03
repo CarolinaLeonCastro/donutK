3e6bb11ac308be7475731657c5c6c38f714ce8e2
Carolina leon <leoncarolina35@gmail.com>
2023-05-03 10:52:39 +0200

    modifs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ShowAll from "./pages/ShowAll";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ShowData from "./pages/ShowData";
 import ShowOne from "./pages/ShowOne";
 import LandingPage from "./pages/LandingPage";
-import GoogleLoginPage from "./pages/GoogleLoginPage";
-import TestCheckout from "./component/TestCheckout";
 import Cart from "./pages/Cart";
 
 import Address from "./pages/Address";
+
 
 function App() {
   return (
@@ -16,13 +15,12 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/googlelogin" element={<GoogleLoginPage />}></Route>
           <Route path="/showall" element={<ShowAll />}></Route>
           <Route path="/Cart" element={<Cart />}></Route>
           <Route path="/showone" element={<ShowOne />}></Route>
           <Route path="/showdata" element={<ShowData />}></Route>
-          <Route path="/TestCheckout" element={<TestCheckout />}></Route>
-          <Route path="/adress" element={<Address />}></Route>
+          <Route path="/adress" element={<Address/>}></Route>
+          
         </Routes>
       </div>
     </Router>
--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import { Box } from '@chakra-ui/react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 import AddressForm from '../component/AddressForm';
+import '../App.css'
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripe = loadStripe('pk_test_HvEeju8Kg8pqDFSjQQyyxGDb');
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+const stripePromise = loadStripe('https://donutk-backend-pifrn.ondigitalocean.app/');
 
 function Address() {
   const options = {
     // Fully customizable with appearance API.
-    appearance: {/*...*/},
+    appearance: {
+      theme: 'stripe',
+      variables: {
+       
+       
+        colorPrimary: '#F687B3',
+        colorIconTabSelected: '#ffff',
+        spacingGridRow: '16px',
+      },
+      rules: {
+        '.Tab, .Input, .Block, .CheckboxInput, .CodeInput': {
+          boxShadow: '0px 3px 10px rgba(18, 42, 66, 0.08)',
+        },
+        '.Block': {
+          borderColor: 'transparent',
+        },
+        '.BlockDivider': {
+          backgroundColor: '#ebebeb',
+        },
+        '.Tab, .Tab:hover, .Tab:focus': {
+          border: '0',
+        },
+        '.Tab--selected, .Tab--selected:hover': {
+          backgroundColor: '#f360a6',
+          color: '#fff',
+        },
+      },
+    },
   };
 
   return (
-    <Elements stripe={stripe} options={options}>
-      <AddressForm />
-    </Elements>
+    <Box maxWidth="600px" mx={{base:"4",md:"auto",xl:"auto"}} mt={{base:"4",md:"8",xl:"12"}} fontFamily={'Gloria Hallelujah'}>
+      <Elements stripe={stripePromise} options={options}>
+        <AddressForm />
+      </Elements>
+    </Box>
   );
-};
+}
 
 export default Address;
