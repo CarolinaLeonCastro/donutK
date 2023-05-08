9fb2e3cce0ffbadfd3e7e0601c1acaa0d793a0e0
Carolina leon <leoncarolina35@gmail.com>
2023-05-08 17:01:39 +0200

    success & cancel pages

--- src/pages/Cancel.tsx
+++ src/pages/Cancel.tsx
@@ -1,54 +1,44 @@
-import {
-    Button,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-  } from "@chakra-ui/react";
-  import { useState } from "react";
-  import { Link } from "react-router-dom";
-  
-  import "../App.css";
-  
-  function Cancel() {
-    const [isOpen, setIsOpen] = useState(true);
-  
-    const handleClose = () => setIsOpen(false);
-  
-    return (
-      <Modal isOpen={isOpen} onClose={handleClose}>
-        <ModalOverlay />
-        <ModalContent >
-          <ModalHeader
-            fontFamily={"Gloria Hallelujah"}
-            textColor={"red.400"}
-            fontSize={"2xl"}
-          >
-            Cancelled Command
-          </ModalHeader>
-          <ModalBody>
-            <p>Your command was cancelled. We're sorry to see you go!</p>
-          </ModalBody>
-  
-          <ModalFooter>
-            <Link to="/showall">
-              <Button
-                bgColor={"pink.300"}
-                rounded={"full"}
-                mr={2}
-                onClick={handleClose}
-                fontFamily={"Gloria Hallelujah"}
-              >
-                Continue Shopping
-              </Button>
-            </Link>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    );
-  }
-  
-  export default Cancel;
-  
+import { Box, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+import "../App.css";
+
+function Cancel() {
+ 
+
+  return (
+    <Box 
+      maxW="full"
+      
+      fontSize="xl"
+      textAlign="center"
+      mx={"auto"}
+      fontFamily={"Gloria Hallelujah"}
+      mt={20}
+      
+
+      >
+      <p> Your command was cancelled. We're sorry to see you go!</p>
+      <Link to="/">
+        <Button
+          bgColor={"pink.300"}
+          rounded={"full"}
+          mt={4}
+          mr={2}
+          fontFamily={"Gloria Hallelujah"}
+        >
+          Try again
+        </Button>
+      </Link>
+    </Box>
+  );
+}
+
+export default Cancel;
+
+
+
+
+
+
+
--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,54 +1,36 @@
-import {
-  Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-} from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-
 import "../App.css";
 
 function Success() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleClose = () => setIsOpen(false);
-
   return (
-    <Modal isOpen={isOpen} onClose={handleClose}>
-      <ModalOverlay />
-      <ModalContent bgColor={"cyan.100"}>
-        <ModalHeader
-          fontFamily={"Gloria Hallelujah"}
-          textColor={"orange.300"}
-          fontSize={"2xl"}
-        >
-          Successful Command
-        </ModalHeader>
-        <ModalBody>
-          <p>Your command was successful. Thank you for shopping with us!</p>
-        </ModalBody>
+    <Box
+      maxW="full"
+      fontSize="xl"
+      textAlign="center"
+      mx="auto"
+      fontFamily={"Gloria Hallelujah"}
+      mt={20}
+      
 
-        <ModalFooter>
-          <Link to="/showall">
-            <Button
-              bgColor={"pink.300"}
-              rounded={"full"}
-              mr={2}
-              onClick={handleClose}
-              fontFamily={"Gloria Hallelujah"}
-             
-            >
-              Continue Shopping
-            </Button>
-          </Link>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+      >
+      <p>Your command was successful. Thank you for shopping with us!</p>
+      <Link to="/showall">
+        <Button
+          bgColor={"pink.300"}
+          rounded={"full"}
+          mt={4}
+          mr={2}
+          fontFamily={"Gloria Hallelujah"}
+        >
+          Continue Shopping
+        </Button>
+      </Link>
+    </Box>
   );
 }
 
 export default Success;
+
+
+
